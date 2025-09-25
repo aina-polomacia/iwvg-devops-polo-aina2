cf4fe83d3ccb712ed3cc1334471086ba85d2eb66
aina-polomacia <aina.polomacia@alumnos.upm.es>
2025-09-26 01:09:35 +0200

    implemented tests for new functionalities of Fraction #11

--- src/test/java/es/upm/miw/devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/devops/code/FractionTest.java
@@ -41,6 +41,74 @@ class FractionTest {
     }
 
     @Test
+    void testIsProper() {
+        Fraction proper = new Fraction(2, 3);
+        Fraction improper = new Fraction(5, 4);
+
+        assertThat(proper.isProper()).isTrue();
+        assertThat(improper.isProper()).isFalse();
+    }
+
+    @Test
+    void testIsImproper() {
+        Fraction proper = new Fraction(2, 3);
+        Fraction improper = new Fraction(5, 4);
+
+        assertThat(proper.isImproper()).isFalse();
+        assertThat(improper.isImproper()).isTrue();
+    }
+
+    @Test
+    void testIsEquivalent() {
+        Fraction f1 = new Fraction(1, 2);
+        Fraction f2 = new Fraction(2, 4);
+        Fraction f3 = new Fraction(3, 4);
+
+        assertThat(f1.isEquivalent(f2)).isTrue();
+        assertThat(f1.isEquivalent(f3)).isFalse();
+    }
+
+    @Test
+    void testAdd() {
+        Fraction f1 = new Fraction(1, 2);
+        Fraction f2 = new Fraction(1, 3);
+        Fraction sum = f1.add(f2);
+
+        assertThat(sum.getNumerator()).isEqualTo(5);
+        assertThat(sum.getDenominator()).isEqualTo(6);
+    }
+
+    @Test
+    void testMultiply() {
+        Fraction f1 = new Fraction(2, 3);
+        Fraction f2 = new Fraction(3, 4);
+        Fraction product = f1.multiply(f2);
+
+        assertThat(product.getNumerator()).isEqualTo(6);
+        assertThat(product.getDenominator()).isEqualTo(12);
+    }
+
+    @Test
+    void testDivide() {
+        Fraction f1 = new Fraction(2, 3);
+        Fraction f2 = new Fraction(4, 5);
+        Fraction quotient = f1.divide(f2);
+
+        assertThat(quotient.getNumerator()).isEqualTo(10);
+        assertThat(quotient.getDenominator()).isEqualTo(12);
+    }
+
+    @Test
+    void testDivideByZeroFractionThrows() {
+        Fraction f1 = new Fraction(1, 2);
+        Fraction zeroNumerator = new Fraction(0, 5);
+
+        assertThatThrownBy(() -> f1.divide(zeroNumerator))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Cannot divide by zero fraction");
+    }
+
+    @Test
     void testToString() {
         Fraction fraction = new Fraction(2, 3);
         String str = fraction.toString();
