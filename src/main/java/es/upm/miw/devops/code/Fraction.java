975d73652769542aced648b781cd6aefa070576a
aina-polomacia <aina.polomacia@alumnos.upm.es>
2025-09-26 00:57:04 +0200

    added changes in Fraction to pass tests #10

--- src/main/java/es/upm/miw/devops/code/Fraction.java
+++ src/main/java/es/upm/miw/devops/code/Fraction.java
@@ -1,4 +1,15 @@
 package es.upm.miw.devops.code;
 
 public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction() {
+        this(1, 1);
+    }
 }
